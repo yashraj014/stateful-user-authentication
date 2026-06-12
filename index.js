6aff198bd5f51ff3a109278795a7b3bd76f731e9
Yash Raj <kumaryashraj21@gmail.com>
2026-06-12 18:41:17 +0530

    added jwt auth

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
 import express from 'express'
-import session from 'express-session';
-import connectMongoDBSession from 'connect-mongodb-session'
+// import session from 'express-session';
+// import connectMongoDBSession from 'connect-mongodb-session'
 import mongoose from 'mongoose'
 import bcrypt from 'bcryptjs';
-
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
 import {User} from './models/user.js';
 const mongoURI = "mongodb://localhost:27017/sessions";
-
+const SECRET_KEY = 'f948d1406a8a44264a2ea842b54708cfac49f24443fe6d34688e5eb777f09e31d9410e33';
 const app = express()
 
 mongoose.connect(mongoURI)
 .then(()=>console.log("MongoDB connected"))
 
-const MongoDBStore = connectMongoDBSession(session);
+// const MongoDBStore = connectMongoDBSession(session);
 
-const store = new MongoDBStore({
-    uri: mongoURI,
-    collection: 'mySessions'
-});
-app.use(session({
-    secret: "a secret key that will sign the session",
-    resave: false,
-    saveUninitialized: false,
-    store: store
-}))
+// const store = new MongoDBStore({
+//     uri: mongoURI,
+//     collection: 'mySessions'
+// });
+// app.use(session({
+//     secret: "a secret key that will sign the session",
+//     resave: false,
+//     saveUninitialized: false,
+//     store: store
+// }))
 
 app.set("view engine","ejs")
-app.use(express.urlencoded({extended:false}))
-
-const isAuth =(req,res,next)=>{
-   if(req.session.userId){
-    next();
-   }
-   else{
-    res.redirect('/login')
-   }
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+// const isAuth =(req,res,next)=>{
+//    if(req.session.userId){
+//     next();
+//    }
+//    else{
+//     res.redirect('/login')
+//    }
+// }
+const isAuthorized = (req,res,next)=>{
+    const token = req.cookies.token;
+    try{
+        const decoded = jwt.verify(token,SECRET_KEY);
+        console.log(decoded);
+        req.userId = decoded.userId;
+        next();
+    }
+    catch(err){
+        return res.status(401).send("Unauthorized");
+    }
 }
 
 app.get("/",(req,res)=>{
@@ -59,7 +72,9 @@ app.post("/signup",async(req,res)=>{
         username,email,password:hashedPw
     })
 
+    
     res.redirect('/login')
+    // res.status(201).json({token:token})
 })
 
 app.get("/login",(req,res)=>{
@@ -77,18 +92,37 @@ app.post("/login",async(req,res)=>{
     const isMatch = await bcrypt.compare(password,user.password);
     if(!isMatch)
         return res.redirect('/login')
-    req.session.userId = user._id;
+    // req.session.userId = user._id;
+    const token = jwt.sign(
+        {userId : user._id},
+        SECRET_KEY,
+        {expiresIn:"1h"}
+    );
+    console.log(token);
+    res.cookie("token", token, {
+    httpOnly: true
+});
     res.redirect('/dashboard')
 })
 
-app.get("/dashboard",isAuth,(req,res)=>{
-    res.render("dashboard")
+app.get("/dashboard",isAuthorized,async(req,res)=>{
+    const user = await User.findOne({
+        _id:req.userId
+    })
+    console.log(req.userId)
+    console.log(user)
+    
+    res.render("dashboard",{user:user})
 })
 
 app.post('/logout',(req,res)=>{
-    req.session.destroy((err)=>{
-        if(err) throw err;
-        res.redirect('/')
-    })
+    // req.session.destroy((err)=>{
+    //     if(err) throw err;
+    //     res.redirect('/')
+    // })
+    res.clearCookie("token", {
+    httpOnly: true,
+});
+res.redirect('/')
 })
 app.listen(8000,()=> console.log("App running on PORT:8000"))
